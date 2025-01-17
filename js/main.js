60cf520f346f16cb56d2d42bf9ca50e08d4f2ead
Ivana Vkvc <ivanavankovic@MacBook-Air-de-Ivana.local>
2025-01-17 13:36:19 +0100

    Adding GSAP Files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// GSAP Modules
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+
+// gsap demo animation
+
+gsap.to("#demo",{
+    rotation : 360,
+    duration : 2
+})
