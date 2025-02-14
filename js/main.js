83f35cea9a126782abbe7526a94b1fadefb9ecdb
Ivana Vkvc <ivanavankovic@MacBook-Air-de-Ivana.local>
2025-02-14 13:51:01 +0100

    Adding gsap animations and section 4 and 5

--- js/main.js
+++ js/main.js
@@ -12,7 +12,19 @@ mm.add({
 
     // Section 2 : Albums
     const tl = gsap.timeline();
-    tl.from(".album", {xPercent: -100})
+    tl.from(".album", {xPercent: +50})
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: ".album",
+        start: "top top",
+        end: "+2000px",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+    })
+
+
     gsap.from(".album", { opacity: 0, y: 100, duration: 1 });
 }) 
 
