9e87846c8e370ada151ee6d0f9a18f9d7e1785b3
Ivana Vkvc <ivanavankovic@MacBook-Air-de-Ivana.local>
2025-02-14 13:40:49 +0100

    Adding section 4 and 5 and some animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 // GSAP Modules
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
-// gsap demo animation
-gsap.from(".album", { opacity: 0, y: 100, duration: 1 });
+// GSAP Animations
+// GSAP MatchMedia
+let mm = gsap.matchmedia();
+mm.add({
+    isMobile: '(max-width: 799px)', // Version Smartphone
+    isDesktop: '(min-width: 800px)', // Version Desktop
+}, (context) => {
+    let {isMobile,isDesktop} = context.conditions;
+
+    // Section 2 : Albums
+    const tl = gsap.timeline();
+    tl.from(".album", {xPercent: -100})
+    gsap.from(".album", { opacity: 0, y: 100, duration: 1 });
+}) 
+
+
+Draggable.create(".movies",{
+    type: "x,y",
+
+    onDrag: function(){
+        document.querySelector('.movies').style.width = "250px";
+    },
+    onDragEnd: function(){
+        document.querySelector('.movies').style.width = "auto";
+    }
+})
