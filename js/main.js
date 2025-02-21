83277953b68ac042b454706e32a52bf6ddba9181
Ivana Vkvc <ivanavankovic@MacBook-Air-de-Ivana.local>
2025-02-21 09:29:02 +0100

    Adding section 5 and animations on 1 3 and 5

--- js/main.js
+++ js/main.js
@@ -3,39 +3,78 @@ gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPl
 
 // GSAP Animations
 // GSAP MatchMedia
-let mm = gsap.matchmedia();
+let mm = gsap.matchMedia();
 mm.add({
     isMobile: '(max-width: 799px)', // Version Smartphone
     isDesktop: '(min-width: 800px)', // Version Desktop
 }, (context) => {
     let {isMobile,isDesktop} = context.conditions;
 
-    // Section 2 : Albums
-    const tl = gsap.timeline();
-    tl.from(".album", {xPercent: +50})
+    // Section 1 : Rendre les titres Draggable
 
-    ScrollTrigger.create({
-        animation: tl,
-        trigger: ".album",
-        start: "top top",
-        end: "+2000px",
-        scrub: true,
-        pin: true,
-        anticipatePin: 1,
+    Draggable.create(".tracks",{
+        type: "x,y",
+    
+        onDrag: function(){
+            document.querySelector('.tracks').style.width = "250px";
+        },
+        onDragEnd: function(){
+            document.querySelector('.tracks').style.width = "50%";
+        }
     })
+    
+    // Section 2 : Albums
 
 
-    gsap.from(".album", { opacity: 0, y: 100, duration: 1 });
-}) 
+    // Section 3 : Plus d'infos
 
-
-Draggable.create(".movies",{
-    type: "x,y",
-
-    onDrag: function(){
-        document.querySelector('.movies').style.width = "250px";
-    },
-    onDragEnd: function(){
-        document.querySelector('.movies').style.width = "auto";
-    }
+    const image = document.querySelector(".image-cg");
+    const hiddenText = document.querySelector(".paragraph");
+    let isVisible = false;
+    
+    image.addEventListener("click", () => {
+      if (!isVisible) {
+        gsap.to(hiddenText, { 
+          opacity: 1, 
+          maxHeight: 500, 
+          duration: 0.5, 
+          ease: "power2.out" 
+        });
+      } else {
+        gsap.to(hiddenText, { 
+          opacity: 0, 
+          maxHeight: 0, 
+          duration: 0.5, 
+          ease: "power2.in" 
+        });
+      }
+      isVisible = !isVisible;
+    });
 })
+
+// Section 4 : Films où Donald Glover est présent : Timeline en décalage
+
+let tl = gsap.timeline({
+    repeat: -1,
+    yoyo: true,
+})
+
+tl.to('.movies', {x :100, stagger : 0.1,})
+.to('movies', {y :100, stagger : 0.1,})
+.to('movies', {x :-100, stagger : 0.1,})
+.to('movies', {x :-100, stagger : 0.1,})
+
+
+// Section 5 : Films que Donald Glover a réalisé : effet de mouvement léger
+
+document.addEventListener('DOMContentLoaded', function () {
+    gsap.to(".realisations", {
+        x: 80,
+        y: -10,
+        rotation: 2,
+        duration: 4,
+        ease: "power1.inOut",
+        yoyo: true,
+        repeat: -1
+    });
+});
